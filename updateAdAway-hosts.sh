3b54eb86086ddccd6943173c38149ccff1bf2dcc
Peter Hindes <ph9214@gmail.com>
2017-09-14 12:32:54 -0600

    First Publish

---
+++ updateAdAway-hosts.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+srclst="hostssources.lst"
+#touch "$srclst"
+
+if [[ $(grep -n "# Start of patch marker &5644 #" /etc/hosts | head -1 | cut -d \: -f 1) && grep -n "# End of patch marker &5844 #" /etc/hosts | tail -1 | cut -d \: -f 
+1 ]]; then
+	upd=true
+	echo Updateing
+	startop=$(expr $(grep -n "# Start of patch marker &5644 #" /etc/hosts | head -1 | cut -d \: -f 1) - 1)
+	endop=$(expr $(grep -n "# End of patch marker &5844 #" /etc/hosts | tail -1 | cut -d \: -f 1) - 1)
+else
+	upd=false
+	echo Running First Install
+	startop=$(awk 'END { print NR }' /etc/hosts)
+	endop=$(awk 'END { print NR }' /etc/hosts)
+fi
+
+if [ -f /etc/hosts ] ; then
+	head -$startop /etc/hosts >> "$srclst"
+	if $upd; then
+		tail -$(expr $endop - $(awk 'END { print NR }' /etc/hosts)) /etc/hosts >> "$srclst"
+	fi
+fi
+
+echo "# Start of patch marker &5644 #" >> "$srclst"
+curl -L "http://adaway.org/hosts.txt" >> "$srclst"
+curl -L "http://hosts-file.net/ad_servers.asp" >> "$srclst"
+curl -L "http://winhelp2002.mvps.org/hosts.txt" >> "$srclst"
+#For blocking youtube flash ads
+echo "0.0.0.0       s.ytimg.com" >> "$srclst"
+echo "# End of patch marker &5844 #" >> "$srclst"
+
+sudo cp "$srclst" /etc/hosts
+
+echo "'hosts' file has been updated successfully..."
+rm "$srclst"
