8588e40f61f49c9f8972620794b9cccbb14eb6d6
Peter Hindes <ph9214@gmail.com>
2017-09-14 13:50:47 -0600

    Working on rearanging flow controll

--- updateAdAway-hosts.sh
+++ updateAdAway-hosts.sh
@@ -2,20 +2,33 @@
 echo "Not ready for use, please check back soon"
 exit
 
+# File to store the new version beffore applying the patch.
 srclst="hostssources.lst"
 #touch "$srclst"
 
+
 if [[ $(grep -n "# Start of patch marker &5644 #" /etc/hosts | head -1 | cut -d \: -f 1) && grep -n "# End of patch marker &5844 #" /etc/hosts | tail -1 | cut -d \: -f
 1 ]]; then
-	upd=true
-	echo Updateing
+	patchExists=true
 	startop=$(expr $(grep -n "# Start of patch marker &5644 #" /etc/hosts | head -1 | cut -d \: -f 1) - 1)
 	endop=$(expr $(grep -n "# End of patch marker &5844 #" /etc/hosts | tail -1 | cut -d \: -f 1) - 1)
+elif [[ $(grep -n "# Start of patch marker &5644 #" /etc/hosts | head -1 | cut -d \: -f 1) && grep -n "# End of patch marker &5844 #" /etc/hosts | tail -1 | cut -d \: -f
+1 ]]; then
+
+# Check if the hosts file has already been patched and if so cut out the exitsing patch to splice in the updated one
+# if [[ $(grep -n "# Start of patch marker &5644 #" /etc/hosts | head -1 | cut -d \: -f 1) && grep -n "# End of patch marker &5844 #" /etc/hosts | tail -1 | cut -d \: -f 1 ]]; then
+
+if patchExists; then
+	echo "Updating hosts file that has been previously patched"
+	finstring=""
+
 
 	if [ -f /etc/hosts ] ; then
 		head -$startop /etc/hosts >> "$srclst"
 		tail -$(expr $endop - $(awk 'END { print NR }' /etc/hosts)) /etc/hosts >> "$srclst"
 	fi
+
+
 
 else
 	upd=false
@@ -37,5 +50,5 @@ echo "# End of patch marker &5844 #" >> "$srclst"
 sudo cp /etc/hosts /etc/hosts.old
 sudo cp "$srclst" /etc/hosts
 
-echo "'hosts' file has been updated successfully..."
+echo "The file at '/etc/hosts' has been patched successfully..."
 rm "$srclst"
